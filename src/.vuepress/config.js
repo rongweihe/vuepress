a54bc6eb0f7ea75cc16b5d1787e015e2697827b4
rongweihe <herongwei@bytedance.com>
2023-04-08 11:23:48 +0800

    feat:update config.js add handbook

--- src/.vuepress/config.js
+++ src/.vuepress/config.js
@@ -11,6 +11,25 @@ module.exports = {
                     { text: '知乎', link: 'https://zhihu.com/people/herongwei' }
                 ]
             }
-        ]
+        ],
+        sidebar: [
+            {
+                title: '欢迎学习',
+                path: '/',
+                collapsable: false, // 不折叠
+                children: [
+                    { title: "学前必读", path: "/" }
+                ]
+            },
+            {
+              title: "基础学习",
+              path: '/handbook/read01',
+              collapsable: false, // 不折叠
+              children: [
+                { title: "条件类型", path: "/handbook/read01" },
+                { title: "泛型", path: "/handbook/read02" }
+              ],
+            }
+          ]
     }
 }
