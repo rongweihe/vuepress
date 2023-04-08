9789783a501d06ecaa02431bf4ae236f7a53550b
rongweihe <herongwei@bytedance.com>
2023-04-08 11:17:57 +0800

    feat:update config.js add themeConfig

--- src/.vuepress/config.js
+++ src/.vuepress/config.js
@@ -1,4 +1,16 @@
 module.exports = {
-    title: 'Hello VuePress',
-    description: 'Just playing around'
+    title: 'ChatGPT中文导航大全',
+    description: 'Just playing around',
+    themeConfig: {
+        nav: [
+            { text: '首页', link: '/' },
+            { 
+                text: '联系作者', 
+                items: [
+                    { text: 'Github', link: 'https://github.com/rongweihe' },
+                    { text: '知乎', link: 'https://zhihu.com/people/herongwei' }
+                ]
+            }
+        ]
+    }
 }
